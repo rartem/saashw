34d77db1cad174fdf6c2d01fb504cdf5d2c840a1
Artem Rudnitsky <artem.rudnitsky@gmail.com>
2012-03-12 10:17:53 +0300

    task3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,19 @@
 class MoviesController < ApplicationController
 
+helper_method :title_class
+helper_method :date_class
+
+def title_class
+  return @@title_class
+end
+
+def date_class
+  return @@date_class
+end
+   
+  @@title_class = 'none'
+  @@date_class = 'none' 
+   
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -7,7 +21,19 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    case params[:sort]
+    when "title"  then 
+      @movies = Movie.order('title asc').all
+      @@title_class = 'hilite'
+      @@date_class = 'none'
+    when "date" then 
+      @movies = Movie.order('release_date asc').all
+      @@title_class = 'none'
+      @@date_class = 'hilite'
+    else @movies = Movie.all
+      @@title_class = 'none'
+      @@date_class = 'none'
+    end
   end
 
   def new
